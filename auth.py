3d7848bc62709c01f000773dfa59c411403d6256
Greatcard11 <cardstels@gmail.com>
2026-05-08 09:18:21 +0100

    Create auth.py

---
+++ auth.py
@@ -0,0 +1,10 @@
+import bcrypt
+
+
+def hash_password(password):
+    return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+
+def verify_password(password, hashed):
+    return bcrypt.checkpw(password.encode(), hashed)
