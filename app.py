425b8844998c1d727d042f61d5d91b96cca0702d
Vivian Iyaha <90201480+vivianiyaha@users.noreply.github.com>
2026-05-15 12:32:34 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,15 @@ import streamlit as st
 import pandas as pd
 import os
 from datetime import datetime
+from pathlib import Path
 
 # =====================================================
 # PAGE CONFIG
 # =====================================================
-st.set_page_config(layout="wide")
-initial_sidebar_state="collapsed"
+st.set_page_config(
+    layout="wide",
+    initial_sidebar_state="collapsed"
+)
 
 # =====================================================
 # DATA FOLDER
